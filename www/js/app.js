69eb75f948d98987959b2491053d6ee9a78778f5
Atsushi Nakatsugawa <atsushi@moongift.jp>
2017-03-07 16:08:55 +0900

    Remove onsen ui

--- www/js/app.js
+++ www/js/app.js
@@ -7,71 +7,76 @@ var onDeviceReady = function() {
     storageBucket: "STORAGE_BUCKET",
     messagingSenderId: "MESSAGING_SENDER_ID"
   };
-  firebase.initializeApp(config);
-  
-  // Vueの処理 
+	firebase.initializeApp(config);
+
+	// Vueの処理
 	var vm = new Vue({
 	  el: '#app',  // マウントするDOM
 	  // 初期データの設定
-	  data: {
-	  	user: {
-	  		isLoggedIn: false,
-	  		mailAddress: "",
-	  		password: ""
-	  	}
-	  },
+		data: {
+		  user: {
+		    isLoggedIn: false,
+		    mailAddress: "",
+		    password: ""
+		  }
+		},
+
 	  // デプロイ完了時のイベント
-	  created: function() {
-	  	var me = this;
-	  	firebase.auth().onAuthStateChanged(function(user) {
-	  		me.user.isLoggedIn = (user !== null);
-	  	});
-	  },
+		created: function() {
+		  // ユーザのステータスが変わったら通知
+		  var me = this;
+		  firebase.auth().onAuthStateChanged(function(user) {
+		    me.user.isLoggedIn = (user !== null);
+		  });
+		},
+
+	    
 	  // テンプレート
-	  template: `
-    <div>
-      <div class="center"> Firebase認証 </div>
-	    <section style="margin: 10px;" v-if="user.isLoggedIn">
-	    	<p>{{ user.mailAddress }}</p>
+		template: `
+		<div>
+	    <div class="center"> Firebase認証 </div>
+		  <section style="margin: 10px;" v-if="user.isLoggedIn">
+		    <p>{{ user.mailAddress }}</p>
 		    <section style="margin: 10px;">
 		      <button @click="logout">ログアウト</button>
 		    </section>
-	    </section>
-			<section v-else style="margin: 10px;">
-	      <p>メールアドレス</p>
-	      <p>
-	        <input v-model="user.mailAddress" placeholder="メールアドレス" />
-	      </p>
-	      <p>パスワード</p>
-	      <p>
-	        <input v-model="user.password" placeholder="パスワード" type="password" />
-	      </p>
-	      <button @click="register">新規登録</button>
-	      <button @click="login">ログイン</button>
-	    </section>
-	  </div>`,
+		  </section>
+		  <section v-else style="margin: 10px;">
+		    <p>メールアドレス</p>
+		    <p>
+		      <input v-model="user.mailAddress" placeholder="メールアドレス" />
+		    </p>
+		    <p>パスワード</p>
+		    <p>
+		      <input v-model="user.password" placeholder="パスワード" type="password" />
+		    </p>
+		    <button @click="register">新規登録</button>
+		    <button @click="login">ログイン</button>
+		  </section>
+		</div>`,
+
 	  // イベント処理
-    methods: {
-	  	// 登録処理
-	  	register: function() {
-	  		firebase.auth().createUserWithEmailAndPassword(this.user.mailAddress, this.user.password)
-	  			.catch(function(error) {
-        		alert(error.message);
-	      	});
-	  	},
-	  	// ログイン処理
-	  	login: function() {
-	  		firebase.auth().signInWithEmailAndPassword(this.user.mailAddress, this.user.password)
-	  			.catch(function(error) {
-	  				alert(error.message);
-	      	});
-	  	},
-	  	// ログアウト処理
-	  	logout: function() {
-	  		firebase.auth().signOut();
-	  	}
+	  methods: {
+			// 登録処理
+			register: function() {
+			  firebase.auth().createUserWithEmailAndPassword(this.user.mailAddress, this.user.password)
+			    .catch(function(error) {
+			      alert(error.message);
+			    });
+			},
+			// ログイン処理
+			login: function() {
+			  firebase.auth().signInWithEmailAndPassword(this.user.mailAddress, this.user.password)
+			    .catch(function(error) {
+			      alert(error.message);
+			    });
+			},
+			// ログアウト処理
+			logout: function() {
+			  firebase.auth().signOut();
+			}
 	  }
 	});
-};
 
+};
 document.addEventListener(window.cordova ?"deviceready" : "DOMContentLoaded", onDeviceReady, false);
